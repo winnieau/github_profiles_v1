777664c7bde285e8e158b03dd88c2194fe8ff08c
Winifred Au <winifredau1990@gmail.com>
2015-09-08 12:10:18 -0400

    can make http requests and tests

--- js/gitUserSearchController.js
+++ js/gitUserSearchController.js
@@ -1,3 +1,12 @@
-githubUserSearch.controller('GitUserSearchController', [function () {
+githubUserSearch.controller('GitUserSearchController', ['$resource', function ($resource) {
 
+  var self = this;
+  var searchResource = $resource('https://api.github.com/search/users');
+
+  self.doSearch = function() {
+    self.searchTerm;
+    self.searchResult = searchResource.get(
+      { q: self.searchTerm }
+    );
+  };
 }]);
--- test/gitUserSearchController.spec.js
+++ test/gitUserSearchController.spec.js
@@ -10,4 +10,36 @@ describe('GitUserSearch', function() {
     expect(ctrl.searchResult).toBeUndefined();
     expect(ctrl.searchTerm).toBeUndefined();
   });
+
+  describe('when searching for a user', function() {
+    var items = [
+      {
+        "login": "tansaku",
+        "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
+        "html_url": "http://github.com/tansaku"
+      },
+      {
+        "login": "stephenlloyd",
+        "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
+        "html_url": "https://github.com/stephenlloyd"
+      }
+    ];
+
+    var httpBackend;
+    beforeEach(inject(function($httpBackend) {
+      httpBackend = $httpBackend
+      httpBackend
+        .when("GET", "https://api.github.com/search/users?q=tansaku")
+        .respond(
+          {items: items}
+        );
+    }));
+
+    it('displays search results', function() {
+      ctrl.searchTerm = 'tansaku';
+      ctrl.doSearch();
+      httpBackend.flush();
+      expect(ctrl.searchResult.items).toEqual(items);
+    });
+  });
 });
