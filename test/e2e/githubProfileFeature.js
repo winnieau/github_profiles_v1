b1d0dc7d9f76836f755f2e6459cec9ccfacbc350
Josh <Tattsy@hotmail.co.uk>
2015-09-10 11:16:50 +0100

    done mor tsets

--- test/e2e/githubProfileFeature.js
+++ test/e2e/githubProfileFeature.js
@@ -1,16 +1,20 @@
 describe('Github Profile finder', function() {
-  it('has a title', function() {
-    browser.get('http://localhost:8080');
 
+  var searchBox = element(by.model('searchCtrl.searchTerm'));
+  var searchButton = element(by.className('btn'));
+
+  beforeEach(function() {
+    browser.get('http://localhost:8080');
+  });
+
+
+  it('has a title', function() {
     expect(browser.getTitle()).toEqual('Github user search');
   });
 
   it('finds profiles', function() {
-    browser.get('http://localhost:8080');
-
-    element(by.model('searchCtrl.searchTerm')).sendKeys('spike01');
-    element(by.className('btn')).click();
-
-    expect(element(by.binding('user.login')).getText()).toEqual('spike0');
+    searchBox.sendKeys('spike01');
+    searchButton.click();
+    expect(element(by.binding('user.login')).getText()).toEqual('spike01');
   });
 });
